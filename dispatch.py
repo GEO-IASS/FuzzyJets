149fbcf452ec811d456b5738bec63357ff035ba5
Conrad Stansbury <chstan@slac.stanford.edu>
2014-08-11 23:58:43 -0700

    Updated dispatch script to reflect argument passing for clustering scheme.

--- dispatch.py
+++ dispatch.py
@@ -6,22 +6,24 @@ def safe_mkdir(p):
     if not os.path.isdir(p):
         os.makedirs(p)
 
-def file_name(prefix, is_intermediate, size, lw, mu):
+def file_name(prefix, is_intermediate, size, lw, mu, rec):
     lw_flag = "1" if lw else "0"
+    rec_flag = "1" if rec else "0"
     div = "/temp_" if is_intermediate else "/"
-    return prefix + div + str(size) + "s_" + str(mu) + "mu_" + lw_flag
+    return prefix + div + str(size) + "s_" + str(mu) + "mu_" + lw_flag + "lw_" + rec_flag + "rec"
 
 def subprocess_call_bg(args):
     subprocess.call(args)
 
-def submit_fuzzy(mu, size, lw, n_events, i, unique_id):
+def submit_fuzzy(mu, size, lw, rec, n_events, i, unique_id):
     logdir = cwd + 'logs/' + name + "/" + time_postfix + "_bsub_" + str(mu) + "_" + str(unique_id)
     safe_mkdir(logdir)
 
     scratchdir = "/scratch/chstan/" + time_postfix + "_" + str(unique_id)
     safe_mkdir(scratchdir)
     lw_flag = "1" if lw else "0"
-    outfile_name = file_name(scratchdir, True, size, lw, mu) + "_" + str(i) + ".root"
+    rec_flag = "1" if rec else "0"
+    outfile_name = file_name(scratchdir, True, size, lw, mu, rec) + "_" + str(i) + ".root"
     size_s = str(1.0*size / 10)
     submit = ['bsub', '-q', queue,
               '-R', 'select[(!preempt&&rhel60&&cvmfs&&inet)]',
@@ -30,10 +32,8 @@ def submit_fuzzy(mu, size, lw, n_events, i, unique_id):
               "./Fuzzy", "--NEvents", str(n_events), "--OutFile", outfile_name,
               "--NPV", str(mu),  "--Size", str(size_s),
               "--PythiaConfig", pythia_conf,
-              "--LearnWeights", lw_flag, "--Batch", "1"]
-    #t = threading.Thread(target = subprocess_call_bg, args = (submit,))
-    #t.daemon = True
-    #t.start()
+              "--LearnWeights", lw_flag, "--Recombination", rec_flag, 
+              "--Batch", "1"]
     subprocess.call(submit)
 
 workdir = "/u/at/chstan/nfs/summer_2014/ForConrad/"
@@ -50,13 +50,14 @@ n_jobs = 100
 n_jobs_patch = 5
 queue = 'xlong'
 
-name = '500kevts_mu0'
+name = '500kevts_mu0_bothrec'
 outdir = cwd + 'files/' + name + '/' + time_postfix
 safe_mkdir(outdir)
 
 NPVs = [0]
 sizes = [7, 8, 9, 10]
 learnWeights = [False, True]
+recombinations = [False, True]
 
 j = 0
 cleanup_commands = []
@@ -64,23 +65,25 @@ cleanup_commands = []
 for current_mu in NPVs:
     for current_size in sizes:
         for current_lw in learnWeights:
-            out_base = file_name(outdir, False, current_size, current_lw, current_mu)
-            cleanup_base = file_name(outdir, True, current_size, current_lw, current_mu)
-            cleanup_command = "hadd " + out_base + ".root" + " " \
-                              + cleanup_base + "_{0.." + str(n_jobs-1) + "}.root"
-            cleanup_commands.append(cleanup_command)
-            for current_job in range(n_jobs):
-                submit_fuzzy(current_mu, current_size, current_lw, events_per_job, current_job, j)
-                j += 1
+            for current_rec in recombinations:
+                out_base = file_name(outdir, False, current_size, current_lw, current_mu, current_rec)
+                cleanup_base = file_name(outdir, True, current_size, current_lw, current_mu, current_rec)
+                cleanup_command = "hadd " + out_base + ".root" + " " \
+                    + cleanup_base + "_{0.." + str(n_jobs-1) + "}.root"
+                cleanup_commands.append(cleanup_command)
+                for current_job in range(n_jobs):
+                    submit_fuzzy(current_mu, current_size, current_lw, current_rec, events_per_job, current_job, j)
+                    j += 1
 
 outdir = cwd + 'files/' + name + '_patch/' + time_postfix
 safe_mkdir(outdir) 
 for current_mu in NPVs:
     for current_size in sizes:
         for current_lw in learnWeights:
-            for current_job in range(n_jobs_patch):
-                submit_fuzzy(current_mu, current_size, current_lw, events_per_job, current_job, j)
-                j += 1
+            for current_rec in recombinations:
+                for current_job in range(n_jobs_patch):
+                    submit_fuzzy(current_mu, current_size, current_lw, current_rec, events_per_job, current_job, j)
+                    j += 1
 
 with open('clean_scripts/' + time_postfix + '.clscr', 'wb') as outf:
     pickle.dump(cleanup_commands, outf)
